204907be3c968adc1ba07a84e22090d392e0c2f9
JulianoSilva05 <julianoqm@gmail.com>
2021-06-21 16:03:57 -0300

    Adiciona os arquivos

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Juliano Silva";
+var idade = 32;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo!"
+//alert(nome + " tem " + idade + " anos");
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toUpperCase());
+
+//alert(frase.replace("Japão","Brasil"))
